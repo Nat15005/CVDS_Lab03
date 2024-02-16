e1675df0623c31befae6a3f453fa378b8e628cee
Nat <laura.rojas-r@mail.escuelaing.edu.co>
2024-02-15 22:37:46 -0500

    Completo

--- src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import static edu.eci.cvds.tdd.registry.Gender.*;
 
-
 public class RegistryTest {
     private Registry registry = new Registry();
 
